33f69b311bd238ee77ed3cca0f315f7226fd001c
Renier Vargas <renier1989@gmail.com>
2024-07-07 08:49:40 -0500

    Configuracion de variables de entorno y validaciones

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,9 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(3000);
+  await app.listen(envs.port);
+
+  console.log(`Running in port ${envs.port}`);
+  
 }
 bootstrap();
