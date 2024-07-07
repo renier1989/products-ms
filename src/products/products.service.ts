f1e6b141792cbabf256e8fa64a0f6d1e902b32b4
Renier Vargas <renier1989@gmail.com>
2024-07-07 10:22:31 -0500

    Rotornando un Producto por el ID

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,4 +1,9 @@
-import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import {
+  Injectable,
+  Logger,
+  NotFoundException,
+  OnModuleInit,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
@@ -6,7 +11,6 @@ import { PaginatinoDto } from '../common/dto/pagination.dto';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
-
   private readonly logger = new Logger('ProductsService');
 
   onModuleInit() {
@@ -15,34 +19,38 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
   }
 
   create(createProductDto: CreateProductDto) {
-
     return this.product.create({
-      data: createProductDto
-    })
+      data: createProductDto,
+    });
   }
 
-  async findAll(paginatinoDto:PaginatinoDto) {
-    const { page, limit } = paginatinoDto
+  async findAll(paginatinoDto: PaginatinoDto) {
+    const { page, limit } = paginatinoDto;
 
     const totalData = await this.product.count();
-    const lastPage = Math.ceil( totalData/limit );
-
+    const lastPage = Math.ceil(totalData / limit);
 
     return {
       data: await this.product.findMany({
         skip: (page - 1) * limit,
-        take: limit
+        take: limit,
       }),
-      meta:{
+      meta: {
         total: totalData,
         page: page,
-        lastPage: lastPage
-      }
+        lastPage: lastPage,
+      },
     };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.product.findFirst({
+      where: { id },
+    });
+    if (!product) {
+      throw new NotFoundException(`Product with id #${id} not found`);
+    }
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
